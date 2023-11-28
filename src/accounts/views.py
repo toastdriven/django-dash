f80b972fb6b33bbb9f0e72f0a31fd17715fe095d
Daniel Lindsley <daniel@toastdriven.com>
2023-11-28 14:37:53 -0600

    Started adding accounts API views.

---
+++ src/accounts/views.py
@@ -0,0 +1,56 @@
+from django.contrib.auth import (
+    authenticate,
+    login,
+    logout,
+)
+
+from utils.api_tools import (
+    ApiView,
+    DataValidationError,
+)
+
+
+class LoginView(ApiView):
+    def validate(self, data):
+        if "username" not in data:
+            raise DataValidationError("Missing username")
+
+        if "password" not in data:
+            raise DataValidationError("Missing password")
+
+        return data
+
+    def post(self, request, *args, **kwargs):
+        data = self.read_json(request)
+        validated = self.validate(data)
+        user = authenticate(validated["username"], validated["password"])
+
+        if user is None:
+            return self.render_error("Invalid credentials.")
+
+        login(request, user)
+        return self.render(
+            {
+                "success": True,
+            }
+        )
+
+
+class SignUpView(ApiView):
+    def post(self, request, *args, **kwargs):
+        return self.render({})
+
+
+class ResetPasswordView(ApiView):
+    def post(self, request, *args, **kwargs):
+        return self.render({})
+
+
+class LogoutView(ApiView):
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        return self.render(
+            {
+                "success": True,
+            }
+        )
